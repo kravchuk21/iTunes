49508d761613472445f8d757a5edd3e5cf3ad1fb
VladKrauchuk <66268773+VladKrauchuk@users.noreply.github.com>
2020-07-28 16:10:58 +0300

    Video Player

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,21 @@
+// импорт модулей
 import { radioPlayerInit } from "./radioPlayer.js";
 import { musicPlayerInit } from "./musicPlayer.js";
 import { videoPlayerInit } from "./videoPlayer.js";
 
+// обЪявлеие переменных
 const playerBtn = document.querySelectorAll(".player-btn");
 const playerBlock = document.querySelectorAll(".player-block");
-const temp = document.querySelector(".temp");
+const temp = document.querySelector(".temp"); // надпись на главном экране
 
+// удаление класса "active" у всех playerBtn и playerBlock
 const deactivationPlayer = () => {
-  temp.style.display = "none";
+  temp.style.display = "none"; // скрытие надписи на главном экране
   playerBtn.forEach((item) => item.classList.remove("active"));
   playerBlock.forEach((item) => item.classList.remove("active"));
 };
 
+// добавление класса "active" активным кнобкам и блокам
 playerBtn.forEach((btn, index) =>
   btn.addEventListener("click", () => {
     deactivationPlayer();
@@ -20,6 +24,7 @@ playerBtn.forEach((btn, index) =>
   })
 );
 
+// инициализация модулей
 radioPlayerInit();
 musicPlayerInit();
 videoPlayerInit();
--- scripts/videoPlayer.js
+++ scripts/videoPlayer.js
@@ -1,3 +1,63 @@
 export const videoPlayerInit = () => {
-  console.log("Video");
+  // обЪявлеие переменных
+  const videoPlayer = document.querySelector(".video-player");
+  const videoButtonPlay = document.querySelector(".video-button__play");
+  const videoButtonStop = document.querySelector(".video-button__stop");
+  const videoProgress = document.querySelector(".video-progress");
+  const videoTimePassed = document.querySelector(".video-time__passed");
+  const videoTimeTotal = document.querySelector(".video-time__total");
+
+  // смена иконки play / pause
+  const toggleIcon = () => {
+    if (videoPlayer.paused) {
+      videoButtonPlay.classList.remove("fa-pause");
+      videoButtonPlay.classList.add("fa-play");
+    } else {
+      videoButtonPlay.classList.add("fa-pause");
+      videoButtonPlay.classList.remove("fa-play");
+    }
+  };
+
+  // запуск / остановка видео
+  const togglePlay = () => {
+    if (videoPlayer.paused) videoPlayer.play();
+    else videoPlayer.pause();
+  };
+
+  // остановка и перемотка видео в начало
+  const stopPlay = () => {
+    videoPlayer.pause();
+    videoPlayer.currentTime = 0;
+  };
+
+  // добовление 0 перед временем
+  const addZero = (n) => (n < 10 ? "0" + n : n);
+
+  videoPlayer.addEventListener("click", togglePlay);
+  videoButtonPlay.addEventListener("click", togglePlay);
+  videoPlayer.addEventListener("play", toggleIcon);
+  videoPlayer.addEventListener("pause", toggleIcon);
+  videoButtonStop.addEventListener("click", stopPlay);
+  videoPlayer.addEventListener("timeupdate", () => {
+    const currentTime = videoPlayer.currentTime; // текущее время видео
+    const duration = videoPlayer.duration; // полное время видео
+
+    videoProgress.value = (currentTime / duration) * 100;
+
+    let minutePassed = Math.floor(currentTime / 60);
+    let secondsPassed = Math.floor(currentTime % 60);
+    let minuteDuration = Math.floor(duration / 60);
+    let secondsDuration = Math.floor(duration % 60);
+
+    videoTimePassed.textContent = `${addZero(minutePassed)}:${addZero(secondsPassed)}`; // текущее время видео (минуты : секунды)
+    videoTimeTotal.textContent = `${addZero(minuteDuration)}:${addZero(secondsDuration)}`; // полное время видео (минуты : секунды)
+  });
+
+  // перемотка видео
+  videoProgress.addEventListener("change", () => {
+    const duration = videoPlayer.duration;
+    const value = videoProgress.value;
+
+    videoPlayer.currentTime = (value * duration) / 100;
+  });
 };
